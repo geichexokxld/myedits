61403cb2993830c9887c98975c3b5c37c47ddec4
geichexokxld <126546836+geichexokxld@users.noreply.github.com>
2023-03-04 00:11:43 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -36,20 +36,13 @@ def stats(update, context):
         last_commit = 'No UPSTREAM_REPO'
     stats = f'<b>Commit Date</b>: {last_commit}\n\n'\
             f'<b>Bot Uptime</b>: {get_readable_time(time() - botStartTime)}\n'\
-            f'<b>OS Uptime</b>: {get_readable_time(time() - boot_time())}\n\n'\
             f'<b>Total Disk Space </b>: {get_readable_file_size(total)}\n'\
             f'<b>Used</b>: {get_readable_file_size(used)} | <b>Free</b>: {get_readable_file_size(free)}\n\n'\
-            f'<b>Upload</b>: {get_readable_file_size(net_io_counters().bytes_sent)}\n'\
-            f'<b>Download</b>: {get_readable_file_size(net_io_counters().bytes_recv)}\n\n'\
+            f'<b>Up</b>: {get_readable_file_size(net_io_counters().bytes_sent)}\n'\
+            f'<b>Down</b>: {get_readable_file_size(net_io_counters().bytes_recv)}\n\n'\
             f'<b>CPU</b>: {cpu_percent(interval=0.5)}%\n'\
             f'<b>RAM</b>: {memory.percent}%\n'\
             f'<b>DISK</b>: {disk}%\n\n'\
-            f'<b>Physical Cores</b>: {cpu_count(logical=False)}\n'\
-            f'<b>Total Cores</b>: {cpu_count(logical=True)}\n\n'\
-            f'<b>SWAP</b>: {get_readable_file_size(swap.total)} | <b>Used</b>: {swap.percent}%\n'\
-            f'<b>Memory Total</b>: {get_readable_file_size(memory.total)}\n'\
-            f'<b>Memory Free</b>: {get_readable_file_size(memory.available)}\n'\
-            f'<b>Memory Used</b>: {get_readable_file_size(memory.used)}\n'
     sendMessage(stats, context.bot, update.message)
 
 def start(update, context):
